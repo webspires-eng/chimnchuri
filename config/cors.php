0f663abb93a18b46c226f500104eecf77d02e672
Akif Ullah <akifullah0317@gmail.com>
2026-02-24 13:45:20 +0500

    Update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ["https://chimnchuri-frontend.vercel.app", "http://localhost:3000", "http://localhost:5173", "*"],
+    'allowed_origins' => ["https://chimnchuri-frontend.vercel.app", "https://chimnchuri-frontend.vercel.app/*", "http://localhost:3000", "http://localhost:5173", "*"],
 
     'allowed_origins_patterns' => [],
 
